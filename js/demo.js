a4237e273ee21cdb4dfbd52140ff36033d6f1aad
MominAhmed21 <mominahmed212006>
2023-11-05 21:45:30 -0800

    add code

--- js/demo.js
+++ js/demo.js
@@ -1,17 +1,15 @@
-const readMoreButton = document.getElementById('read-more');
-const contentDiv = document.getElementById('content');
+document.addEventListener("DOMContentLoaded", function() {
+    const boxes = document.querySelectorAll(".box");
 
-readMoreButton.addEventListener('click', () => {
-    // Load the content for the new page (front-end developer details)
-    contentDiv.innerHTML = `
-        <h1>Front-End Developer</h1>
-        <p>Front-end developers are responsible for creating the user interface and user experience of websites and web applications. They work with HTML, CSS, and JavaScript to build interactive and visually appealing websites.</p>
-        <button id="back-button">Back</button>
-    `;
+    function checkScroll() {
+        boxes.forEach(box => {
+            const boxTop = box.getBoundingClientRect().top;
+            if (boxTop < window.innerHeight) {
+                box.classList.add(box.getAttribute("data-animate"));
+            }
+        });
+    }
 
-    const backButton = document.getElementById('back-button');
-    backButton.addEventListener('click', () => {
-        // Go back to the original page
-        contentDiv.innerHTML = '';
-    });
+    window.addEventListener("scroll", checkScroll);
+    checkScroll();
 });
